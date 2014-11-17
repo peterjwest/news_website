f300f407c6bb3d5a448d8ac0c74064bcbf239d95
Peter West <pete@whiteoctober.co.uk>
2014-11-17 20:31:26 +0000

    Added home route/template

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000);
