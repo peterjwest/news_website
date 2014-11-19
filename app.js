9672fb14d940eac74f0d34e926e0a12e0ac2a624
Peter West <pete@whiteoctober.co.uk>
2014-11-19 00:20:10 +0000

    Added RSS feed parser module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var rssParser = require('./lib/rss-parser');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+var feeds = ['http://feeds.bbci.co.uk/news/rss.xml', 'http://feeds.skynews.com/feeds/rss/home.xml'];
 
 app.use(bodyParser.urlencoded({extended: true}));
 
