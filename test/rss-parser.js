908f48d74f2015658d1733321e4d50c19900d1ae
Peter West <pete@whiteoctober.co.uk>
2014-11-19 13:34:31 +0000

    Improved parser tests to check for specified limit

--- test/rss-parser.js
+++ test/rss-parser.js
@@ -1,6 +1,6 @@
-var streamSpec = require('stream-spec');
 var assert = require('assert');
 var nock = require('nock');
+var streamSpec = require('stream-spec');
 var rssParser = require('../lib/rss-parser');
 
 describe('RSS parser module', function () {
@@ -62,7 +62,7 @@ describe('RSS parser module', function () {
       .get('/xml')
       .replyWithFile(200, __dirname + '/fixtures/rss2.xml');
 
-    var expected = ['Title one', 'Title two', 'Title three', 'Title A', 'Title B', 'Title C'];
+    var expected = ['Title A','Title B','Title C','Title one','Title two','Title three'];
     var items = [];
 
     rssParser(['http://tapir.com/rss', 'http://badger.com/xml'], false)
@@ -74,7 +74,36 @@ describe('RSS parser module', function () {
           return item.title;
         });
 
-        assert.deepEqual(titles.sort(), expected.sort());
+        assert.deepEqual(titles, expected);
+
+        done();
+      });
+  });
+
+  it('should parse and stream items up to the specified limit', function (done) {
+    nock('http://tapir.com')
+      .get('/rss')
+      .delay(100)
+      .replyWithFile(200, __dirname + '/fixtures/rss.xml');
+
+    nock('http://badger.com')
+      .get('/xml')
+      .delay(150)
+      .replyWithFile(200, __dirname + '/fixtures/rss2.xml');
+
+    var expected = ['Title one', 'Title A'];
+    var items = [];
+
+    rssParser(['http://tapir.com/rss', 'http://badger.com/xml'], 1)
+      .on('data', function (data) {
+        items.push(data);
+      })
+      .on('end', function () {
+        var titles = items.map(function(item) {
+          return item.title;
+        });
+
+        assert.deepEqual(titles, expected);
 
         done();
       });
