9bfeb52c43052875efdf10d7e463a844adb39ed3
Peter West <pete@whiteoctober.co.uk>
2014-11-19 00:31:00 +0000

    Replaced jade templates with streaming handlebar

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var rssParser = require('./lib/rss-parser');
+var templateStream = require('./lib/template-stream')(__dirname + '/views', 'layout_.handlebars')
+
 var app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 var feeds = ['http://feeds.bbci.co.uk/news/rss.xml', 'http://feeds.skynews.com/feeds/rss/home.xml'];
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-    res.render('index');
+app.get('/', function (req, res) {
+    res.locals.title = 'The newest news';
+    var articles = rssParser(feeds, 10);
+    templateStream('article.handlebars', res.locals, articles, res);
 });
 
 app.use(express.static('public'));
